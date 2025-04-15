2cb3177824229c324b20945f1d747510c7b5fec3
wintun12 <winmin.tun@nhs.net>
2025-04-15 13:19:10 +0100

    update source code to compatible with windows

--- README.md
+++ README.md
@@ -1,31 +1,38 @@
 # pre_basher_filter
-
-This script is desinged to run on Windows machine. It can be run as a stanalone python script or wrapped into the .exe file. 
+The tool has two mode (option1 and option2). Option1 converts all rhchp files (if not specify) in the given dir to txt files by running the MSV.CNGenotypeExportTool. The converted txt files are filtered to remove the unwanted snp using the reference excel sheet "HT-CMA hg38 genome coverage.xlsx". The filtered txt files are combined into one txt file which is to be used as Basher input. Option2 uses already converted single txt file as an input and filters out the unwanted SNP.
 
 To run standalone python script, download the github repo from https://github.com/moka-guys/pre_basher_filter
 
-To run the script on Windows --> python filter.py -d <path/to/dir/for/rhchp_files>
+To run option1 --> `python filter.py -d </dir/for/rhchp_files>`
+To run option2 --> `python filter.py -d </dir/for/rhchp_files> -m opiton2 -i <txt file name>`
 
-To build into .exe --> python -m PyInstaller --onefile --add-data "<file/path/to/MSVtool>;." .\filter.py. This will generate the filter.exe in "dist" folder
 
-To run exe file --> .\dist\filter.exe -d <path/to/dir/for/rhchp_files>
+The tool has optional input args to filter by chromosome and POS. For that, use the flag -c (for selected chromosome), --start and --end for POS. 
 
-The tool has optional input args to filter by chromosome and POS. For that use the flag -c (for seleted chromosome), --start and --end for POS. 
-
-***How the tool works***
-The tool converts all rhchp files in the given dir to txt file by running the MSV tool. The converted txt files are filtered to remove the bad snp using the reference excel sheet "HT-CMA hg38 genome coverage.xlsx". The filtered txt files are combined into one txt file which is to be used as Basher output.
-
-***Refrence files required***
-- CytoScan_HTCMA_96.na36.r3.a1.annot.db - required for converting the rhchp file to txt file
-- MSV.CNGenotypeExportTool.exe - thermofisher tool used to convert the rhchp file to txt file
-- HT-CMA hg38 genome coverage.xlsx - refernece SNP file to filter bad SNP
+***Requirements***
+- CytoScan_HTCMA_96.na36.r4.a1.annot.db - required for converting the rhchp file to txt file.
+- MSV.CNGenotypeExportTool.exe (tool from Thermofisher)
+- HT-CMA hg38 genome coverage.xlsx - reference file used to filter the unwanted SNP
 
 ***Input***
 - file dir - str to direct the folder where rhchp files are located (-d or --file_dir)
+- mode to run - str to choose mode (optional for option1) (-m or --mode) 
 - chromosome number - int/str for chromosome to filter (optional) (-c or --chr)
-- start - int for start position to filter (opitional) (--start)
+- start - int for start position to filter (optional) (--start)
 - end - int for end position to filter (optional) (--end)
+- ref file - str to direct the file path for reference file (optional) (-r, --snp_ref)
+- msv tool - str to direct the path for msv tool (optional) (--msv)
+- annot file - str to direct the path for annot file (optional) (-a or --annot)
+- included files - list of file(s) to include to filter (compulsory in option2) (-i or --included)
+
+***Note*** if the optional inputs are not provided, either default values are taken or the input args are not used.
 
 ***Output***
-- combined filtered txt file to be used as a Basher input
+These two ouput files are genearted in the subfolder named "filtered_array_output"
+- combined filtered txt file to be used as a BASHer input
 - log file 
+Note: if option1 is used, the converted txt files are generated in the same dir as rhchp files.
+
+Please see more detailed doc here https://seglh.atlassian.net/wiki/spaces/~63bc0b8427c8920a5c01e2e6/pages/380862466/How+to+run+pre_basher_filter+on+Windows
+
+
--- filter.py
+++ filter.py
@@ -3,85 +3,33 @@ import sys
 import subprocess
 import argparse
 import logging
+from datetime import datetime
 import pandas as pd
+#import config
+
 logger = logging.getLogger(__name__)
-
-
-def get_log(log_file) -> None:
-    """
-    Setup for log file
-        Return: None
-    """
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-    console_handler = logging.StreamHandler()
-    file_handler = logging.FileHandler(log_file) # need to update 'D:/Downloads/wmt/basher_filter_log.log'
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    console_handler.setFormatter(formatter)
-    file_handler.setFormatter(formatter)
-
-    # Add the handlers to the logger
-    logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
-
-
-def conversion(file_dir, msv_tool) -> list:
-    """
-    Convert the rhchp file to txt file using MSV tool from Thermofisher
-        Return: list of converted files
-    """
-    check_exist(msv_tool)
-    annot_file = "C:/Users/Public/Documents/RhAS/Library/CytoScan_HTCMA_96.r3.1/CytoScan_HTCMA_96.na36.r3.a1.annot.db"
-    check_exist(annot_file)
-    converted_files = []
-    all_files = os.listdir(file_dir)
-    rhchp_files = [item for item in all_files if item.endswith('.rhchp') and
-                   os.path.isfile(os.path.join(file_dir, item))]
-    if len(rhchp_files) < 1:
-        logger.error("There is no rhchp file in the given folder")
-        sys.exit(1)
-    for rhchp in rhchp_files:
-        rhchp_file = os.path.join(file_dir, rhchp)
-        file_name = os.path.splitext(os.path.basename(rhchp))[0]
-        txt_file_name = os.path.join(file_dir, file_name)
-        powershell_command = f"{msv_tool} --input {rhchp_file} --output {txt_file_name}.txt" #update D:\Downloads\wmt\MSV.CNGenotypeExportTool\MSV.CNGenotypeExportTool.exe
-
-        # Run PowerShell through subprocess
-        process = subprocess.Popen(["powershell", "-Command",
-                                   powershell_command],
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE,
-                                   text=True)
-        # Get the output and errors (if any)
-        output, error = process.communicate()
-        if output:
-            converted_files.append(f"{file_name}.txt")
-            logger.info(f"{rhchp} is successfully converted into {file_name}.txt")
-        if error:
-            print("Error:\n", error)
-            logger.error(f"Error while converting {rhchp} to txt file")
-    logger.info(f"total {len(converted_files)} rhchp files are converted to txt file")
-    return converted_files
+now = datetime.now()
+datetimestr = now.strftime("%Y%m%d%H%M%S") + f"{now.microsecond // 1000:03}"
 
 
 def get_arguments() -> argparse.Namespace:
     """
-    Uses argparse module to define and handle command line input arguments
+    Uses argparse to define and handle command line input arguments
     and help menu
         Return argparse.Namespace (object): Contains the parsed arguments
     """
     parser = argparse.ArgumentParser(
         description=(
-            "Given an input rhchp file(s), will convert them into the txt file(s) and  "
-            "the unwanted SNP are filtered out"
+            "Given an input rhchp file(s), convert them into "
+            "the txt file(s) and the unwanted SNP are filtered out"
         ),
-        usage="Used to filter SNP array filter before inputting into BASHER app",
+        usage="Used to filter SNP array file before inputting into BASHer",
         )
     parser.add_argument(
         "-d",
         "--file_dir",
         required=True,
-        help="Dir to rhchp file(s)",
+        help="dir to rhchp file(s)",
         )
     parser.add_argument(
         "-c",
@@ -94,60 +42,142 @@ def get_arguments() -> argparse.Namespace:
         )
     parser.add_argument(
         "--end",
-        help="chromosome start to filter",
-        )    
-    parser.add_argument(
-        "-L",
-        "--log",
-        default="D:/Downloads/wmt/basher_filter_log.log",
-        help="path to log file",
+        help="chromosome end to filter",
         )
     parser.add_argument(
         "--msv",
-        default='D:/Downloads/wmt/MSV.CNGenotypeExportTool/MSV.CNGenotypeExportTool.exe',
+        default="S:/Genetics/Bioinformatics/Software/pre-basher/MSV.CNGenotypeExportTool/MSV.CNGenotypeExportTool.exe",
         help="path to MSV tool",
-        )  
+        )
     parser.add_argument(
         "-r",
         "--snp_ref",
-        default="D:/Downloads/HT-CMA hg38 genome coverage.xlsx",
+        default="S:/Genetics/DNA LAB/Current/PGD/pgd results/SNP array/Excels & BED files/HT-CMA hg38 genome coverage.xlsx",
         help="path to ref file to filter snp",
+        )
+    parser.add_argument(
+        "-a",
+        "--annot",
+        default="S:/Genetics/Bioinformatics/Software/pre-basher/CytoScan_HTCMA_96.na36.r4.a1.annot.db",
+        help="path to annot file for MSV tool",
+        )
+    parser.add_argument(
+        "-i",
+        "--included",
+        type=str,
+        nargs="+",
+        help="specify file(s) to be included, required if mode is option2",
+        )
+    parser.add_argument(
+        "-m",
+        "--mode",
+        default="option1",
+        help=("mode of filtering, either option1 or option2. "
+              "option1 converts and filters using rhchp as input. "
+              "option2 filters txt file as input"),
         )
     return parser.parse_args()
 
 
-def filter_combine(args, **kwargs) -> None:
+def get_log(file_dir) -> None:
     """
-    Filter the array file(s) by removing unwanted SNP and combine all files into a single txt file
+    Setup for log file
+        Return: None
+    """
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler()
+    outdir = os.path.join(file_dir, "filtered_array_output")
+    log_file = os.path.join(outdir, "pre_basher_filter_log.log")
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    file_handler = logging.FileHandler(log_file)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+    # Add the handlers to the logger
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+
+def conversion(args) -> list:
+    """
+    Convert the rhchp file to txt file using MSV tool from Thermofisher
+        Return: list of converted files
+    """
+    check_exist(args.msv)
+    annot_file = args.annot
+    check_exist(annot_file)
+    converted_files = []
+    all_files = os.listdir(args.file_dir)
+
+    if args.included is not None:
+        rhchp_files = args.included
+    else:
+        rhchp_files = [item for item in all_files if item.endswith(".rhchp")
+                       and os.path.isfile(os.path.join(args.file_dir, item))]
+    if len(rhchp_files) < 1:
+        logger.error("There is no rhchp file in the given folder")
+        sys.exit(1)
+    for rhchp in rhchp_files:
+        rhchp_file = os.path.join(args.file_dir, rhchp)
+        file_name = os.path.splitext(os.path.basename(rhchp))[0]
+        txt_file_name = os.path.join(args.file_dir, file_name)
+        powershell_command = (f"{args.msv} --input {rhchp_file} "
+                              f"--output {txt_file_name}.txt "
+                              f"--annotation {annot_file}")
+        logger.info(f"Command line to convert: {powershell_command}")
+        # Run PowerShell through subprocess
+        convert = subprocess.Popen(["powershell", "-Command",
+                                   powershell_command],
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE,
+                                   text=True)
+        output, error = convert.communicate()
+        if output:
+            converted_files.append(f"{file_name}.txt")
+            logger.info(f"{rhchp} is successfully converted into "
+                        f"{file_name}.txt")
+        if error:
+            print("Error while converting {rhchp} to txt file", error)
+            logger.error(f"Error while converting {rhchp} to txt file")
+            sys.exit(1)
+    logger.info(f"Total {len(converted_files)} rhchp files are converted to "
+                f"txt file. The files converted are {converted_files}")
+    return converted_files
+
+
+def filter_combine(args, txt_files) -> None:
+    """
+    Filter the array file(s) by removing unwanted SNP, and
+    combine all files into a single txt file (in option1)
     """
     check_exist(args.snp_ref)
-    good_snp_df = pd.read_excel(args.snp_ref, sheet_name="Filtered SNPs good data set",
-                                usecols=['probeset_id']) # update "D:/Downloads/HT-CMA hg38 genome coverage.xlsx" , "Chr", "Position"
-    try:
-        converted_files
-    except:
-        all_files = os.listdir(args.file_dir)
-        txt_files = [item for item in all_files if item.endswith('.txt') and os.path.isfile(os.path.join(args.file_dir, item))]
-        if len(txt_files) < 1:
-            logger.error("There is no txt file in the given folder")
-            sys.exit(1)
-        print("not using converted append file")
-    else:
-        txt_files = converted_files
-        print("used converted append files")
+    good_snp_df = pd.read_excel(args.snp_ref,
+                                sheet_name="Filtered SNPs good data set",
+                                usecols=["probeset_id"])
     dfs = []
     n = 1
     sanity_check = []
     for array in txt_files:
-        array_df = pd.read_csv(os.path.join(args.file_dir, array), sep="\t", low_memory=False)
-        array_df = array_df[["ProbeSetID", "Chr", "Position", "CallCode"]]
-        array_df.rename({'ProbeSetID': 'probeset_id'}, axis=1, inplace=True)
-        filtered_df = pd.merge(good_snp_df, array_df, on=['probeset_id'], how='inner')
+        array_df = pd.read_csv(os.path.join(args.file_dir, array), sep="\t",
+                               low_memory=False)
+        if args.mode == "option1":
+            array_df = array_df[["ProbeSetID", "Chr", "Position", "CallCode"]]
+            array_df.rename({"ProbeSetID": "probeset_id"}, axis=1, inplace=True)
+        elif args.mode == "option2":
+            array_df.rename({"Probeset ID": "probeset_id"}, axis=1, inplace=True)
+        filtered_df = pd.merge(good_snp_df, array_df, on=["probeset_id"],
+                               how="inner")
         filtered_df.reset_index(drop=True, inplace=True)
-        col_name = os.path.splitext(os.path.basename(array))[0]
-        filtered_df.rename({'Chr_x': 'Chr', 'Position_x': 'Position', "CallCode" : col_name}, axis=1, inplace=True)
-        filtered_df = filtered_df[["probeset_id", "Chr", "Position", col_name]]
-        logger.info(f"{array} has been filtered")
+        if args.mode == "option1":
+            col_name = os.path.splitext(os.path.basename(array))[0] + ".rhchp"
+            filtered_df.rename(
+                {"Chr_x": "Chr", "Position_x": "Position",
+                 "CallCode": col_name}, axis=1, inplace=True
+                )
+            filtered_df = filtered_df[["probeset_id", "Chr", "Position", col_name]]
+        logger.info(f"Unwanted SNP are filtered out from {array}")
 
         # filter with chromosome if given
         if args.chr is not None:
@@ -161,38 +191,45 @@ def filter_combine(args, **kwargs) -> None:
         # filter with position if given
         if (args.start is not None) & (args.end is not None):
             filtered_df["Position"] = filtered_df["Position"].astype(int)
-            filtered_pos_df = filtered_df[(filtered_df["Position"] >= int(args.start)) & (filtered_df["Position"] <= int(args.end))]
+            filtered_pos_df = filtered_df[
+                                (filtered_df["Position"] >= int(args.start))
+                                & (filtered_df["Position"] <= int(args.end))
+                                ]
             filtered_pos_df.reset_index(drop=True, inplace=True)
             filtered_df = filtered_pos_df
-            logger.info(f"{array} file is filtered with Position from {args.start} to {args.end}")
-
-        sanity_check.append(len(filtered_df))
+            logger.info(f"{array} file is filtered with Position from "
+                        f"{args.start} to {args.end}")
+        # merge df from each loop
         if n == 1:
             dfs = filtered_df
         else:
-            dfs = pd.merge(dfs, filtered_df[['probeset_id', col_name]], on='probeset_id', how='left')
+            dfs = pd.merge(dfs, filtered_df[["probeset_id", col_name]],
+                           on="probeset_id", how="left")
         n = n+1
-        logger.info("complete filtering")
-
-    # save as txt file
-    outdir = os.path.join(args.file_dir, "filtered_array_output")
-    if not os.path.exists(outdir):
-        os.makedirs(outdir)
-        logger.info("Output folder is created")
-    else:
-        logger.info("Output folder already exists")
-
-    dfs.to_csv(os.path.join(outdir, "combined_filtered_array.txt"), index=False)
-    logger.info("combined filtered array file is saved")
+        sanity_check.append(len(filtered_df))
+        logger.info(f"Complete filtering of {array}")
 
     # do sanity check
     if len(list(set(sanity_check))) > 1:
-        print("check error")
-        logger.error("Error with filtering so the number of SNP in individual files are not consistent")
+        print("Error with filtering so the number of SNP in individual "
+              "files are not consistent")
+        logger.error("Error with filtering so the number of SNP in "
+                     "individual files are not consistent")
         sys.exit(1)
 
+    # save as txt file
+    outdir = os.path.join(args.file_dir, "filtered_array_output")
+    outfile = os.path.basename(os.path.normpath(args.file_dir))
+    dfs.rename({"probeset_id": "Probeset ID"}, axis=1, inplace=True)
+    dfs.to_csv(os.path.join(outdir, outfile + "_" + datetimestr + ".txt"),
+               sep="\t", index=False)
+    logger.info(f"{outfile}_{datetimestr}.txt file is saved in {outdir}")
 
-def check_exist(file_path):
+
+def check_exist(file_path) -> None:
+    """
+    Check if the file path exist
+    """
     if os.path.exists(file_path):
         logger.info(f"{file_path} exists")
     else:
@@ -200,20 +237,31 @@ def check_exist(file_path):
         sys.exit(1)
 
 
-if __name__ == "__main__":
-    print("Current working directory:", os.getcwd())    
-    parsed_args = get_arguments()
-    get_log(parsed_args.log)
-    # convert file to txt
-    #converted_files = conversion(parsed_args.file_dir, parsed_args.msv)
-    converted_files = ["test.txt", "test1.txt"]
-    # filter snp
-    try:
-        converted_files
-    except:
-        print("not using converted append file")
-        filter_combine(parsed_args)        
-    else:
-        print("used converted append file")
-        filter_combine(parsed_args, converted_files=converted_files)
+def main(args):
+    """
+    Call functions to convert and/or filter array files
+    """
+    get_log(args.file_dir)
+    logger.info(f"Command line: {' '.join(sys.argv)}")
 
+    # option1 converts rhchp files to txt and then filters
+    if args.mode == "option1":
+        logger.info("option1 mode is used")
+        converted_files = conversion(args)
+        filter_combine(args, converted_files)
+    # option2 does not convert, just filters
+    elif args.mode == "option2":
+        logger.info("option2 mode is used")
+        if args.included is None or len(args.included) > 1:
+            logger.error("-i empty or more than one file")
+            sys.exit(1)
+        converted_files = args.included
+        filter_combine(args, converted_files)
+    else:
+        logger.error("mode has to be either option1 or option2")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    parsed_args = get_arguments()
+    main(parsed_args)
